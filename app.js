dacdb6ec50aac7135f33703230133bdaf7ed9625
Satyam <satyam94163@gmail.com>
2022-05-20 23:53:09 +0530

    Added blog post removed warning

--- app.js
+++ app.js
@@ -11,15 +11,17 @@ const myBlogSchema = new mongoose.Schema({
 })
 const BlogDb = mongoose.model('blog', myBlogSchema)
 
-function updateBlogs(callback){BlogDb.find({}, (err, blog) => {
-    if (err) {
-        console.log(err)
-    } else {
-        blogs = blog
-    }
-    if (typeof callback == "function")
-        callback();
-})}
+function updateBlogs(callback) {
+    BlogDb.find({}, (err, blog) => {
+        if (err) {
+            console.log(err)
+        } else {
+            blogs = blog
+        }
+        if (typeof callback == "function")
+            callback();
+    })
+}
 updateBlogs()
 
 app.set('view engine', 'ejs')
@@ -28,8 +30,8 @@ app.use(express.static('public'))
 
 app.get('/home', (req, res) => {
     updateBlogs(() => {
-    res.render('home', { blogs: blogs, _: _})
-})
+        res.render('home', { blogs: blogs, _: _ })
+    })
 })
 app.get('/', (req, res) => {
     res.redirect('/home')
@@ -51,10 +53,8 @@ app.get('/:pathName', (req, res) => {
             if (blogTitle === pathName) {
                 res.render('blog', { blog: blog })
             }
-            else{
-                res.send(`<h1>The Blog post is removed !</h1>`)
-            }
         })
+        res.send(`<h1>The Blog post is removed !</h1>`)
     })
 })
 
@@ -77,10 +77,10 @@ app.post('/newBlog', (req, res) => {
 })
 app.post('/del', (req, res) => {
     BlogDb.deleteOne({ _id: req.body.blogId }, (err) => {
-        if(err){
+        if (err) {
             console.log(err)
         }
-        else{
+        else {
             console.log('Delete one blog Successully')
             updateBlogs(() => {
                 res.redirect('/home')
